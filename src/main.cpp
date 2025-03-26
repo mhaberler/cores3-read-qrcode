2d0f03df7198985a565cc3b009eb6d5fcd53c165
Michael Haberler <git@mah.priv.at>
2025-03-26 05:19:55 +0100

    quirc_new() and quirc_resize(): once only

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,8 @@ struct WiFiConfig wcfg;
 
 struct quirc_code *code;
 struct quirc_data *data;
+struct quirc *qr = nullptr;
+
 app_state_t appstate = AS_UNCONFIGURED;
 app_state_t prev_appstate = AS_UNDEFINED;
 
@@ -52,6 +54,7 @@ void chimeSuccess(void) {
         __734443__universfield__system_notification_4_wav_len,
         44100, true);
 }
+
 bool readStoredWiFiConfig() {
     wifi_config_t config;
     esp_err_t err = esp_wifi_get_config(WIFI_IF_STA, &config);
@@ -70,6 +73,7 @@ bool readStoredWiFiConfig() {
     }
     return false;
 }
+
 void setup() {
 
     M5.begin();
@@ -116,7 +120,7 @@ void loop() {
     esp_err_t err;
 
     M5.update();
-     if (CoreS3.BtnPWR.wasClicked()) {
+    if (CoreS3.BtnPWR.wasClicked()) {
 
         canvas.printf("erasing WiFi config\r\n");
         canvas.pushSprite(0, display.height()/2+ VSPACE);
@@ -172,68 +176,69 @@ void loop() {
                                                     (uint8_t *)CoreS3.Camera.fb->buf,
                                                     lgfx::v1::grayscale_8bit,
                                                     TFT_WHITE, TFT_BLACK);
-            struct quirc *qr = quirc_new();
             int width = fb->width;
             int height = fb->height;
+            if (!qr) {
+                // once only - if not needed anymore, free with quirc_destroy(qr)
+                qr = quirc_new();
+                assert(quirc_resize(qr, width, height) >= 0);
+            }
 
-            if (qr && quirc_resize(qr, width, height) >= 0) {
-                uint8_t *image = quirc_begin(qr, &width, &height);
-                if (image) {
-                    memcpy(image, fb->buf, fb->len);
+            uint8_t *image = quirc_begin(qr, &width, &height);
+            if (image) {
+                memcpy(image, fb->buf, fb->len);
 
-                    quirc_end(qr);
-                    int num_codes = quirc_count(qr);
-                    if (num_codes) {
-                        log_i("width %u height %u num_codes %d",
-                              fb->width, fb->height,num_codes);
+                quirc_end(qr);
+                int num_codes = quirc_count(qr);
+                if (num_codes) {
+                    log_i("width %u height %u num_codes %d",
+                          fb->width, fb->height,num_codes);
+                }
+
+                for (int i = 0; i < num_codes; i++) {
+                    quirc_extract(qr, i, code);
+                    quirc_decode_error_t err = quirc_decode(code, data);
+                    if (err == QUIRC_ERROR_DATA_ECC) {
+                        quirc_flip(code);
+                        err = quirc_decode(code, data);
                     }
+                    if (!err) {
+                        chimeSuccess();
 
-                    for (int i = 0; i < num_codes; i++) {
-                        quirc_extract(qr, i, code);
-                        quirc_decode_error_t err = quirc_decode(code, data);
-                        if (err == QUIRC_ERROR_DATA_ECC) {
-                            quirc_flip(code);
-                            err = quirc_decode(code, data);
-                        }
-                        if (!err) {
-                            chimeSuccess();
+                        log_i("payload '%s'", data->payload);
+                        log_i("Version: %d", data->version);
+                        log_i("ECC level: %c", "MLHQ"[data->ecc_level]);
+                        log_i("Mask: %d", data->mask);
+                        log_i("Length: %d", data->payload_len);
+                        log_i("Payload: %s", data->payload);
 
-                            log_i("payload '%s'", data->payload);
-                            log_i("Version: %d", data->version);
-                            log_i("ECC level: %c", "MLHQ"[data->ecc_level]);
-                            log_i("Mask: %d", data->mask);
-                            log_i("Length: %d", data->payload_len);
-                            log_i("Payload: %s", data->payload);
+                        const String payload = String((const char *)data->payload);
 
-                            const String payload = String((const char *)data->payload);
+                        wcfg = parseWiFiQR(payload);
+                        log_i("SSID '%s'", wcfg.SSID.c_str());
+                        log_i("type '%s'", wcfg.type.c_str());
+                        log_i("password '%s'", wcfg.password.c_str());
 
-                            wcfg = parseWiFiQR(payload);
-                            log_i("SSID '%s'", wcfg.SSID.c_str());
-                            log_i("type '%s'", wcfg.type.c_str());
-                            log_i("password '%s'", wcfg.password.c_str());
-
-                            if (wcfg.SSID.length() > 0) {
-                                WiFi.begin(wcfg.SSID.c_str(), wcfg.password.c_str());
-                                WiFi.persistent(true);
-                                appstate = AS_CONNECTING;
-                                canvas.printf("SSID: %s\r\n", wcfg.SSID.c_str());
-                                // canvas.printf("Password: %s\r\n", wcfg.password.c_str());
-                                canvas.pushSprite(0, display.height()/2+ VSPACE);
-                            } else {
-                                canvas.printf("QR: %s\r\n", payload.c_str());
-                                canvas.pushSprite(0, display.height()/2+ VSPACE);
-                            }
-                            delay(3000);
-                        } else {
-                            chimeError();
-                            canvas.printf("decode: %s\r\n",quirc_strerror(err));
+                        if (wcfg.SSID.length() > 0) {
+                            WiFi.begin(wcfg.SSID.c_str(), wcfg.password.c_str());
+                            WiFi.persistent(true);
+                            appstate = AS_CONNECTING;
+                            canvas.printf("SSID: %s\r\n", wcfg.SSID.c_str());
+                            // canvas.printf("Password: %s\r\n", wcfg.password.c_str());
                             canvas.pushSprite(0, display.height()/2+ VSPACE);
-
-                            delay(500);
+                        } else {
+                            canvas.printf("QR: %s\r\n", payload.c_str());
+                            canvas.pushSprite(0, display.height()/2+ VSPACE);
                         }
+                        delay(3000);
+                    } else {
+                        chimeError();
+                        canvas.printf("decode: %s\r\n",quirc_strerror(err));
+                        canvas.pushSprite(0, display.height()/2+ VSPACE);
+
+                        delay(500);
                     }
                 }
-                quirc_destroy(qr);
             }
             CoreS3.Camera.free();
         }
